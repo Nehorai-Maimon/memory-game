89c79c15c9f3ab25a0aea9f3714bfb0ee60b0be9
נהוראי <nehoraimimon@gmail.com>
2022-04-07 23:29:03 +0300

    progres
    1. option of chooze number of cards.
    2 pozition of cards.
    3. functions when hover on cards.
    4. graphics

--- project.js
+++ project.js
@@ -1,23 +1,32 @@
-const cards = ["A", "B", "C", "D", "E", "F"]
+let cards = []
+numOfCards = Number(prompt(
+    `enter number of cards:
+remember, the many cards you have- the game became harder!!
+`));
+// numOfCards = 5
+
+for (let i=0 ; i < numOfCards ; i++){
+    cards[i] = i
+}
 
 function createCard(idx){
     const cardEl=document.createElement("div")
     cardEl.innerHTML=cards[idx];
     cardEl.id=idx
     cardEl.className ="card"
-
+    
     return cardEl
 }
 
 function shuffle (arr){
     arr.sort(() => Math.random() - 0.5);
- }
+}
 
 shuffle(cards);
 
 const board = document.getElementById("board")
- for (i in cards){
+for (i in cards){
     const element = createCard(i)
     board.appendChild(element)
- }
+}
 
