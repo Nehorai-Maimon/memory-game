9fc8c7e0870c34bed649bb2494a920d6480dd99f
נהוראי <nehoraimimon@gmail.com>
2022-04-19 23:11:02 +0300

    all works
    everything just good!!

--- project.js
+++ project.js
@@ -1,32 +1,60 @@
-let cards = []
-numOfCards = Number(prompt(
-    `enter number of cards:
-remember, the many cards you have- the game became harder!!
-`));
-// numOfCards = 5
-
-for (let i=0 ; i < numOfCards ; i++){
-    cards[i] = i
+let cards = [];
+numOfCards = 16;
+for (let i = 1; i <= numOfCards / 2; i++) {
+  cards[i] = i;
+}
+let a = 1;
+for (let i = numOfCards; i > numOfCards / 2; i--) {
+  cards[i] = a;
+  a++;
+}
+let cardTaken = [],
+  takenBool = false,
+  oneCard;
+function createCard(idx) {
+  let cardEl = document.createElement("div");
+  cardEl.id = idx;
+  cardEl.className = "card";
+  cardEl.onclick = () => {
+    cardEl.innerHTML = cards[idx];
+    if (takenBool == false) {
+      cardTaken.push(Number(cardEl.innerText));
+      console.log(cardTaken);
+      takenBool = true;
+      oneCard = cardEl;
+    } else {
+      cardTaken.push(Number(cardEl.innerText));
+      console.log(cardTaken);
+      setTimeout(()=> {
+        if (cardTaken[0] !== cardTaken[1]) {
+          oneCard.innerText = ``;
+          cardEl.innerHTML = ``;
+          cardTaken = [];
+          takenBool = false;
+          // alert(`no match!!🤦‍♂️`);
+          let x = document.getElementById(`lose`)
+          x.play();
+        } else if (cardTaken[0] === cardTaken[1]) {
+          cardTaken = [];
+          takenBool = false;
+          let y = document.getElementById(`win`)
+          y.play();
+          // alert(`match!!🤔`);
+        }
+      }, 500)
+    }
+  };
+  return cardEl;
 }
 
-function createCard(idx){
-    const cardEl=document.createElement("div")
-    cardEl.innerHTML=cards[idx];
-    cardEl.id=idx
-    cardEl.className ="card"
-    
-    return cardEl
-}
-
-function shuffle (arr){
-    arr.sort(() => Math.random() - 0.5);
+function shuffle(arr) {
+  arr.sort(() => Math.random() - 0.5);
 }
 
 shuffle(cards);
 
-const board = document.getElementById("board")
-for (i in cards){
-    const element = createCard(i)
-    board.appendChild(element)
+const board = document.getElementById("board");
+for (i in cards) {
+  const element = createCard(i);
+  board.appendChild(element);
 }
-
