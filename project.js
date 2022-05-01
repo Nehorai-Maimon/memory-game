6ff8652dc8aee3323b48abcac58a4b7287b9f389
נהוראי <nehoraimimon@gmail.com>
2022-05-01 21:02:02 +0300

    upgrade  SELECT avg("LifeExpectancy") FROM Country WHERE "SurfaceArea" > 1000000

--- project.js
+++ project.js
@@ -8,20 +8,26 @@ for (let i = numOfCards; i > numOfCards / 2; i--) {
   cards[i] = a;
   a++;
 }
+
 let cardTaken = [],
   takenBool = false,
-  oneCard, counter = 0;
+  oneCard,
+  thisCard,
+  twoCard,turn = false, winner,
+  counter = 0;
 function createCard(idx) {
   let cardEl = document.createElement("div");
   cardEl.id = idx;
   cardEl.className = "card";
-  cardEl.onclick = () => {
+  cardEl.onclick = function (event) {
     cardEl.innerHTML = cards[idx];
     if (takenBool == false) {
+      thisCard = event.target;
       cardTaken.push(Number(cardEl.innerText));
       takenBool = true;
       oneCard = cardEl;
     } else {
+      twoCard = event.target;
       cardTaken.push(Number(cardEl.innerText));
       setTimeout(() => {
         if (cardTaken[0] !== cardTaken[1]) {
@@ -30,14 +36,22 @@ function createCard(idx) {
           oneCard.innerText = ``;
           cardEl.innerHTML = ``;
           cardTaken = [];
+          turn = !turn
           takenBool = false;
         } else if (cardTaken[0] === cardTaken[1]) {
           let y = document.getElementById(`win`);
           y.play();
           cardTaken = [];
           takenBool = false;
-          counter += 2
-          if (counter === 16){
+          counter += 2;
+          if(turn == false){
+            winner = player1
+          } else{
+            winner = player2
+          }
+          trueAns(winner)
+          hideCard()
+          if (counter === 16) {
             let z = document.getElementById(`finish`);
             z.play();
           }
@@ -60,3 +74,19 @@ for (i in cards) {
   board.appendChild(element);
 }
 
+const player1 = document.querySelector(`#player1`);
+const player2 = document.querySelector(`#player2`);
+
+function trueAns(winner){
+  let won1 = document.createElement(`div`);
+  won1.className = `winners`
+  won1.innerHTML = thisCard.innerHTML
+  winner.append(won1)
+}
+
+function hideCard(){
+  thisCard.className = "hiddenCard";
+  twoCard.className = "hiddenCard";
+  twoCard.innerHTML = "";
+  oneCard.innerHTML = "";
+}
